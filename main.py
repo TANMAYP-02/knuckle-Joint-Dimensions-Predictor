eec33c2a1f5b7be5d1c1eac1774bf6e2157acd43
Tanmay <tanupatil2305@gmail.com>
2025-09-21 19:04:20 +0530

    done

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import numpy as np
+from flask_cors import CORS
+import joblib
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})  # allow all origins
+
+# Load trained model
+model = joblib.load("D:\\College\\TY\\Sem1\\ML\\tanmay\\rf_knuckle_joint_model.pkl")
+cols_y = ['d1', 'd2', 'd3', 't', 't1', 't2']
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json() 
+        load = float(data['load'])
+        fos_pin = float(data['fos_pin']) 
+        fos_eye = float(data['fos_eye'])
+        print("Inputs:", load, fos_pin, fos_eye)
+    except (KeyError, ValueError, TypeError):
+        return jsonify({'error': 'Invalid input'}), 400
+
+    arr = np.array([[load, fos_pin, fos_eye]])
+    pred = model.predict(arr)[0]
+    result = {col: round(val, 2) for col, val in zip(cols_y, pred)}
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
